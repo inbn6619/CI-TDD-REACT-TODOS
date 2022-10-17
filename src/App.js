11b7979ee52f883bfadc6c2e59c03f8e3ce5f825
inbn6619 <inbn6619@gmail.com>
2022-10-17 15:49:05 +0900

    todoform ver 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoForm from './Todoform';
+import TodoForm from './TodoForm';
 
 function App() {
   return (
--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,22 +1,22 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback } from 'react';
 
-const TodoForm = ({onInsert}) => {
-    const [value, setValue] = useState('')
+const TodoForm = ({ onInsert }) => {
+    const [value, setValue] = useState('');
     const onChange = useCallback(e => {
         setValue(e.target.value);
-        // console.log(value)
-    }, [])
+        // console.log(value);
+    }, []);
     const onSubmit = useCallback(e => {
-        setValue('')
-        e.preventDefault();
-    }, [onInsert, value])
+        onInsert(value);
+        setValue('');
+        e.preventDefault(); // 새로고침 방지
+    }, [onInsert, value]);
     return (<div>
         <form onSubmit={onSubmit}>
             <input value={value} onChange={onChange} placeholder="할 일을 입력하세요" />
             <button type="submit">등록</button>
         </form>
-    </div>
-    )
-}
+    </div>);
+};
 
 export default TodoForm;
