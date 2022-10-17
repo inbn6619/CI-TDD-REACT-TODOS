7f6b955b0de53f90ec5b79ec3cbe6c5cf1ea9cba
inbn6619 <inbn6619@gmail.com>
2022-10-17 15:30:48 +0900

    TodoForm Submit event test ok

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
-const TodoForm = () => {
+const TodoForm = ({onInsert}) => {
+    const [value, setValue] = useState('')
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+        // console.log(value)
+    }, [])
+    const onSubmit = useCallback(e => {
+        setValue('')
+        e.preventDefault();
+    }, [onInsert, value])
     return (<div>
-        <form>
-            <input PlaceholderText="할 일을 입력하세요" />
+        <form onSubmit={onSubmit}>
+            <input value={value} onChange={onChange} placeholder="할 일을 입력하세요" />
             <button type="submit">등록</button>
         </form>
-    </div>)
+    </div>
+    )
 }
 
 export default TodoForm;
